d4e1293d89159e60f9a588ca8b8cda6930a647a9
ka <insideka01@gmail.com>
2022-11-18 00:15:57 +0900

    11.17 State in Compose - 9. State hoisting

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -5,21 +5,38 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, onIncrement = { count++ }, modifier)
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        Text(
-            text = "You've had $count glasses."
-        )
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
         Button(
-            onClick = { count++ },
-            modifier = Modifier.padding(top = 8.dp)
+            onClick = onIncrement,
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10
         ) {
             Text(text = "Add one")
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WaterCounterPreview() {
+    StatefulCounter()
 }
